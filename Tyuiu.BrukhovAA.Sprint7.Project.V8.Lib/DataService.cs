0c17134e2a5c4f82f4a19a6ce7e354a61275d00b
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-12-13 21:20:31 +0500

    2

--- Tyuiu.BrukhovAA.Sprint7.Project.V8.Lib/DataService.cs
+++ Tyuiu.BrukhovAA.Sprint7.Project.V8.Lib/DataService.cs
@@ -9,28 +9,54 @@ namespace Tyuiu.BrukhovAA.Sprint7.Project.V8.Lib
 {
     public class DataService
     {
-        public string[,] LoadFromFileData(string filePath)//метод, который открывает матрицу из файла по заданному пути
+        public string[,] LoadFromFileData(string filePath)//метод, который загружает матрицу из файла по заданному пути
         {
             string fileData = File.ReadAllText(filePath);
 
-            fileData = fileData.Replace('\n', '\r');
-            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            fileData = fileData.Replace('\n', '\r');//меняем переход на новую строку на каретку
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);//Разделяем строки по кареткам и закидываем в массив
 
             int rows = lines.Length;
-            int colums = lines[0].Split(';').Length;
+            int colums = lines[0].Split(';').Length;//находим количество элементов в строке с разделение в виде ;
 
-            string[,] array = new string[rows, colums];
+            string[,] DataBase = new string[rows, colums];
 
             for (int r = 0; r < rows; r++)
             {
-                string[] line_r = lines[r].Split(';');
+                string[] line_r = lines[r].Split(';');//помещаем значения из строк в массив по разделителю
                 for (int c = 0; c < colums; c++)
                 {
-                    array[r, c] = line_r[c];
+                    DataBase[r, c] = line_r[c];//заполняем им матрицу
                 }
             }
 
-            return array;
+            return DataBase;
+        }
+        public string SearchForString(string[,] DataBase, string SearchingData)//метод, который возращает строку, если в ней находится искомое значение
+        {
+            int rows = DataBase.GetUpperBound(0) + 1;
+            int cols = DataBase.Length / rows;
+            string[] person = new string[rows];
+            string res = "";
+            string str = "";
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    str += DataBase[r, c] + " ";
+                }
+                person[r] = str + ";";
+                str = "";
+            }
+            foreach(string p in person)
+            {
+                if(p.Contains(SearchingData))
+                {
+                    res += p;
+                }
+
+            }
+            return res;
         }
     }
 }
--- Tyuiu.BrukhovAA.Sprint7.Project.V8.Test/DataServiceTest.cs
+++ Tyuiu.BrukhovAA.Sprint7.Project.V8.Test/DataServiceTest.cs
@@ -8,19 +8,28 @@ namespace Tyuiu.BrukhovAA.Sprint7.Project.V8.Test
     [TestClass]
     public class DataServiceTest
     {
+        DataService ds = new DataService();
+        string filePath = @"C:\Users\bruho\OneDrive\Рабочий стол\Программирование шаблоны\Матрица с водителями.csv";
         [TestMethod]
         public void ValidLoadFromFileData()
         {
-            DataService ds = new DataService();
-            string filePath = @"C:\Users\bruho\OneDrive\Рабочий стол\Программирование шаблоны\Матрица с водителями.csv";
-
-            string[,] waitArr = new string[4, 7] { { "1", "Иванов", "Андрей", "Петрович", "20.02.1993", "7", "50400" }, 
-                                                   { "2", "Петров", "Василий", "Ибрагимович", "14.06.1982", "12", "65300" },
-                                                   { "3", "Сувовор", "Павел", "Максимович", "08.08.1995", "6", "44320" }, 
-                                                   { "4", "Павлов", "Иван", "Евгеньевич", "26.03.2000", "3", "40080" } };
+            string[,] waitArr = new string[4, 7] { { "ТН-1", "Иванов", "Андрей", "Петрович", "20.02.1993", "7", "50400" }, 
+                                                   { "ТН-2", "Петров", "Василий", "Ибрагимович", "14.06.1982", "12", "65300" },
+                                                   { "ТН-3", "Сувовор", "Павел", "Максимович", "08.08.1995", "6", "44320" }, 
+                                                   { "ТН-4", "Павлов", "Иван", "Евгеньевич", "26.03.2000", "3", "40080" } };
 
             string[,] resArr = ds.LoadFromFileData(filePath);
             CollectionAssert.AreEqual(waitArr, resArr);
+        }
+        [TestMethod]
+        public void ValidSearchForString()
+        {
+
+            string[,] DataBase = ds.LoadFromFileData(filePath);
+            string search = "ТН-2";
+            string res = ds.SearchForString(DataBase, search);
+            string wait = "ТН-2 Петров Василий Ибрагимович 14.06.1982 12 65300 ;";
+            Assert.AreEqual(wait, res);
         }
     }
 }
